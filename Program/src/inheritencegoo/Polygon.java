690ca2813655cd0792273ca874114fd7f9b4a76e
Safkat Khan <joga90zim@gmail.com>
2020-06-16 20:39:20 +0800

    Polygon inherit shape

---
+++ Program/src/inheritencegoo/Polygon.java
@@ -0,0 +1,20 @@
+package inheritencegoo;
+import java . awt . Graphics ;
+
+public class Polygon extends Shape {
+	public Polygon ( int n ) {
+		super ( n ) ;
+		}
+		public Polygon ( int [] x , int [] y , int n ) {
+		super (x , y , n ) ;
+		}
+		public void draw ( Graphics g ) {
+		g . setColor ( getColor () ) ;
+		g . drawPolygon ( getX () , getY () , getNVertices () ) ;
+		}
+		public void fill ( Graphics g ) {
+		g . setColor ( getColor () ) ;
+		g . fillPolygon ( getX () , getY () , getNVertices () ) ;
+		}
+
+}
